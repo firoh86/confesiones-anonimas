77f71fe41919f47f7a77ccf5ee6a3e5096d2479a
alejandro <firohh.as@gmail.com>
2020-08-17 22:05:59 +0200

    preredux

--- src/index.js
+++ src/index.js
@@ -4,21 +4,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.scss';
 
-// firebase npm install reactfire@next soporta hooks
-import { FirebaseAppProvider } from 'reactfire';
-import firebaseconfig from './firebase/fire';
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseAppProvider firebaseCinfig={firebaseconfig}>
-      <Suspense fallback={<p>Calentando los burritos...</p>}>
-        <App />
-      </Suspense>
-    </FirebaseAppProvider>
+    <Suspense fallback={<p>Calentando los burritos...</p>}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
